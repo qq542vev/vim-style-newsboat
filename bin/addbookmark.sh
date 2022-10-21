91d7b28c8f8d0f0c0fa21f136e0f591e856a4ff2
qq542vev <qq542vev@yahoo.co.jp>
2022-10-21 09:22:06 +0900

    Update

--- bin/addbookmark.sh
+++ bin/addbookmark.sh
@@ -550,6 +550,33 @@ htmlTemplate=$(
 )
 awkFunctionScript=$(
 	cat <<-'__EOF__'
+	### Function: getline_exec
+	##
+	## コマンドの実行結果を取得する。
+	##
+	## Parameters:
+	##
+	##   string - 引数となる文字列。
+	##   result - 結果を代入する変数。
+
+	function getline_exec(command, result,  tmpvar,i) {
+		split("", result)
+
+		result["stdout"] = ""
+		command = sprintf("(\n%s\n)\n%s", command, "printf '%3d' \"${?}\"")
+
+		for(i = 1; 0 < (command | getline tmpvar); i++) {
+			result["stdout"] = result["stdout"] (i == 1 ? "" : "\n") tmpvar
+		}
+
+		close(command)
+
+		result["status"] = substr(result["stdout"], length(result["stdout"]) - 2)
+		gsub(/ /, "", result["status"])
+
+		result["stdout"] = substr(result["stdout"], 1, length(result["stdout"]) - 3)
+	}
+
 	### Function: shell_argument
 	##
 	## 安全な Shell Script 用の引数を生成する。
@@ -600,33 +627,6 @@ awkFunctionScript=$(
 		getline_exec(sprintf("%s %s <<%s\n%s\n%s", varargs, command, delimiter, heredoc, delimiter), result)
 
 		return result["stdout"]
-	}
-
-	### Function: getline_exec
-	##
-	## コマンドの実行結果を取得する。
-	##
-	## Parameters:
-	##
-	##   string - 引数となる文字列。
-	##   result - 結果を代入する変数。
-
-	function getline_exec(command, result,  tmpvar,i) {
-		split("", result)
-
-		result["stdout"] = ""
-		command = sprintf("(\n%s\n)\n%s", command, "printf '%3d' \"${?}\"")
-
-		for(i = 1; 0 < (command | getline tmpvar); i++) {
-			result["stdout"] = result["stdout"] (i == 1 ? "" : "\n") tmpvar
-		}
-
-		close(command)
-
-		result["status"] = substr(result["stdout"], length(result["stdout"]) - 2)
-		gsub(/ /, "", result["status"])
-
-		result["stdout"] = substr(result["stdout"], 1, length(result["stdout"]) - 3)
 	}
 	__EOF__
 )
